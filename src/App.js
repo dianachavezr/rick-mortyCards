066049aeebb70c467fff21c815ad5898daa0bf64
DIANACH <dianepatrich@gmail.com>
2022-02-22 18:36:52 -0500

    Se sube proyecto Cards

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
+import Pagination from './components/Pagination/Pagination';
+
+
+function App() {
+  let [pageNumber, setPageNumber] = useState(1)
+
+  let [fetchedData, updateFetchedData] = useState([])
+  let {info, results} = fetchedData;
+
+
+  let api =  `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(()=>{
+
+    (async function(){
+      let data = await fetch(api).then(res=>res.json());
+      updateFetchedData(data)
+
+    })();
+
+  },[api]);
+
+  return (
+    <div className="App">
+      <h1 className="text-center ubuntu my-4">Rick & Morty <span className="text-primary">WiKi</span>
+      </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters/>
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards results={results}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
+    </div>
+  );
+}
+
+export default App;
